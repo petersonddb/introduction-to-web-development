d2144e2cec8ab997f84e99f0028b0c6093a73e25
Peterson Daronch de Bem <petersondaronch@gmail.com>
2020-05-01 23:23:15 +0000

    Add user

---
+++ db/migrate/20200501211826_devise_create_users.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      database_authenticatable(t)
+
+      # # Recoverable
+      # table.string   :reset_password_token
+      # table.datetime :reset_password_sent_at
+
+      # # Rememberable
+      # table.datetime :remember_created_at
+
+      # # Trackable
+      # table.integer  :sign_in_count, default: 0, null: false
+      # table.datetime :current_sign_in_at
+      # table.datetime :last_sign_in_at
+      # table.inet     :current_sign_in_ip
+      # table.inet     :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email, unique: true
+    # add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+
+  private
+
+  def database_authenticatable(table)
+    table.string :email,              null: false, default: ''
+    table.string :encrypted_password, null: false, default: ''
+  end
+end
