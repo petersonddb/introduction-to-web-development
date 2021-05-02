c88f155e53598b5dd1766d0b9c80babe6ea00d24
peterson <petersondaronch@gmail.com>
2021-05-02 16:52:27 -0300

    Add a navigation sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Menu from './Menu/Menu';
 import TickedDates from './TickedDates/TickedDates';
 
 function App() {
   return (
     <main className="app-container">
-      <TickedDates />
+      <Menu className="menu" />
+      <TickedDates className="ticked-dates" />
     </main>
   );
 }
---
+++ src/Menu/Menu.js
@@ -0,0 +1,12 @@
+import './Menu.css';
+const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li className="active">Datas marcadas</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
