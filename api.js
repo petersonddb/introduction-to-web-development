92387957a103d585da31f71ee88d7de26204399c
Peterson Daronch de Bem <petersondaronch@gmail.com>
2021-08-18 22:54:38 -0300

    Add mongoose as database client and strong-params as request params filter

--- api.js
+++ api.js
@@ -1,11 +1,24 @@
-var express = require("express");
-var cors = require('cors');
-var app = express();
+const express = require("express")
+const cors = require('cors')
+const params = require('strong-params')
+const app = express()
+const mongoose = require('mongoose')
 
-app.use(cors({ origin: process.env.ALLOWED_ORIGINS }));
-app.use(express.json());
+app.use(cors({ origin: process.env.ALLOWED_ORIGINS }))
+app.use(express.json())
+app.use(params.expressMiddleware())
 app.use(require('./TickedDates/Controller'))
+app.use(require('./ValidationErrorHandler'))
 
-app.listen(3000, () => {
- console.log("Server running on port 3001");
-});
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const conn = mongoose.connection
+conn.on('error', (err) => console.error(console, err))
+conn.once('open', () => {
+  app.listen(3000, () => {
+    console.log("Server running on port 3000")
+  })
+})
